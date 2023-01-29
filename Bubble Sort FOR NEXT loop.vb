566f8389547ccd09437df11c445b4c3e8973660e
AliArsalanSiddiqui <123864461+AliArsalanSiddiqui@users.noreply.github.com>
2023-01-29 15:24:55 +0500

    Add files via upload

---
+++ Bubble Sort FOR NEXT loop.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NUM(5) As Integer
+        Dim x, y, temp, n As Integer
+
+
+        For x = 1 To 5
+            NUM(x) = 0
+        Next
+
+        For x = 1 To 5
+            Console.Write("Enter Value." & x & ":")
+            temp = Console.ReadLine
+            NUM(x) = temp
+        Next
+        n = 4
+        For x = 1 To 4
+            For y = 1 To n
+                If NUM(y) > NUM(y + 1) Then
+                    temp = NUM(y)
+                    NUM(y) = NUM(y + 1)
+                    NUM(y + 1) = temp
+                End If
+            Next
+            n = n - 1
+        Next
+        For x = 1 To 5
+            Console.WriteLine(NUM(x))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
