ae8567ea63418e0cfc8e42e6de9217ea9b6fd145
AliArsalanSiddiqui <123864461+AliArsalanSiddiqui@users.noreply.github.com>
2023-01-30 17:25:26 +0500

    Add files via upload

---
+++ Stack no 2.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Dim Stack(4) As String
+    Dim stackpt As Integer
+
+    Sub Main()
+
+        Push("Ali")
+        Push("farhan")
+        Push("umer")
+        Push("ahad")
+        Console.WriteLine("stack after push:")
+        Showstack()
+        Console.WriteLine()
+        pop()
+        Console.WriteLine()
+        Console.WriteLine("stack after pop:")
+        Showstack()
+        Console.ReadKey()
+
+    End Sub
+
+    Sub Push(ByVal name As String)
+        If stackpt > 4 Then
+            Console.WriteLine("Stack overflow")
+            Exit Sub
+        End If
+        stackpt = stackpt + 1
+        Stack(stackpt) = name
+    End Sub
+
+    Sub pop()
+        Console.WriteLine("The popped element is:" & Stack(stackpt))
+        Stack(stackpt) = ""
+        stackpt = stackpt - 1
+    End Sub
+
+    Sub Showstack()
+        For x = 1 To 4
+            Console.WriteLine(Stack(x))
+        Next
+    End Sub
+
+End Module
