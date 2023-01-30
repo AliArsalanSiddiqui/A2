6da6f213b121ca0dda7f7bcee0d37d21d38d52c3
AliArsalanSiddiqui <123864461+AliArsalanSiddiqui@users.noreply.github.com>
2023-01-30 15:46:42 +0500

    Update Stack.vb

--- Stack.vb
+++ Stack.vb
@@ -2,15 +2,15 @@ Module Module1
     Dim TopOfStack As Integer
     Dim stack(10) As String
     Dim item As String
+    
     Sub Push(ByVal item As String)
-        If TopOfStack < 9 Then
+        If TopOfStack < stack.Length - 1 Then
             TopOfStack = TopOfStack + 1
             stack(TopOfStack) = item
         Else
             Console.WriteLine("stack is full")
         End If
     End Sub
-
 
     Function pop()
         If TopOfStack = -1 Then
@@ -24,16 +24,21 @@ Module Module1
     End Function
     Sub Main()
         TopOfStack = -1
+        
         'insert any name of your choice in Push()
-        'push can be called as many times as the index of the stack like in this case 10 times
+        'push() can be called as many times as the index of the stack like in this case 10 times
         Call Push("umer")
         Call Push("Ali")
         Call Push("ahad")
+        Call Push("Farhan")
+        Call Push("Hasnain")
         Console.WriteLine("Stack after Push:")
         For x = 0 To 9
             Console.WriteLine(stack(x))
         Next
-
+        
+        'pop() can be called as many times as the index of the stack like in this case 10 times
+        Call pop()
         Call pop()
         Call pop()
         Console.WriteLine("Stack after pop:")
