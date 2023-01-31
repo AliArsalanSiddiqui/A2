fd6769417de7c64177943b9aac33af58d7437b34
AliArsalanSiddiqui <123864461+AliArsalanSiddiqui@users.noreply.github.com>
2023-01-31 15:29:29 +0500

    Add files via upload

---
+++ Insertion Sort.vb
@@ -0,0 +1,49 @@
+﻿Module Module1
+    Dim Score(8) As Integer
+    Dim arraysize, Temp1, Position As Integer
+
+    Sub Main()
+
+        arraysize = 8
+
+        For x = 1 To 8
+            Console.WriteLine("Enter Number" & " " & x & ":")
+            Score(x) = Console.ReadLine
+        Next
+
+        Console.WriteLine()
+        ShowUnsortedArray()
+
+        For Student = 1 To 8
+            Temp1 = Score(Student)
+            Position = Student
+            Do While (Position > 1) And (Score(Position - 1) > Temp1) ' ">" will sort in ascending order while "<" will sort in descending order
+                Score(Position) = Score(Position - 1)
+                Score(Position - 1) = Temp1
+                Position = Position - 1
+            Loop
+        Next
+
+        Console.WriteLine()
+        ShowSortedArray()
+        Console.ReadKey()
+
+    End Sub
+    Sub ShowSortedArray()
+
+        Console.WriteLine("Sorted array :")
+        For x = 1 To 8
+            Console.WriteLine(Score(x))
+        Next
+
+    End Sub
+    Sub ShowUnsortedArray()
+
+        Console.WriteLine("Unsorted array :")
+        For x = 1 To 8
+            Console.WriteLine(Score(x))
+        Next
+
+    End Sub
+
+End Module
